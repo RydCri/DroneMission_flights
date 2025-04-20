e1267f8b39de19b90f57f38c715684d7b16b3d2d
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-20 13:03:07 -0500

    flask-cors

---
+++ backend/app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_cors import CORS
+
+from .extensions import db, migrate
+from .routes.auth import auth as auth_bp
+from .routes.missions import missions as missions_bp
+import os
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    app.config.update(
+        SESSION_COOKIE_SAMESITE='None',
+        SESSION_COOKIE_SECURE=True  # True for HTTPS
+    )
+    # Extensions
+    CORS(app,
+         supports_credentials=True,
+         resources={
+             r"/auth/*": {"origins": "http://localhost:5173"},
+             r"/missions/*": {"origins": "http://localhost:5173"},
+         }
+         )
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(missions_bp, url_prefix='/missions')
+
+    return app
