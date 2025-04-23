0134a415ff559554a6970dd628d51892d2d0c9bc
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-23 06:43:34 -0500

    exportJSON and modal closers set to DOM window

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,2 +1,9 @@
 import "./js/map.js";
 import "./js/missionBuilder.js";
+import "./js/tree.js"
+import { exportMissionJSON , closeBatchEditor, closeWaypointEditor, openBatchEditor} from "./js/tree.js";
+
+window.exportMissionJSON = exportMissionJSON;
+window.closeBatchEditor = closeBatchEditor;
+window.closeWaypointEditor = closeWaypointEditor;
+window.openBatchEditor = openBatchEditor;
