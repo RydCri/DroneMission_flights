760d214799117f6fb36c68d21e40d32b6e0ad5fa
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-24 03:48:22 -0500

    DOM window objects decoupling from waypoints

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,16 @@
 import "./js/map.js";
 import "./js/missionBuilder.js";
 import "./js/tree.js"
-import { exportMissionJSON , closeBatchEditor, closeWaypointEditor, openBatchEditor} from "./js/tree.js";
+import { exportMissionJSON , generateKML, closeBatchEditor, closeWaypointEditor, openBatchEditor} from "./js/tree.js";
+import { missions, waypoints, openWaypointEditor} from "./js/missionBuilder.js";
 
 window.exportMissionJSON = exportMissionJSON;
 window.closeBatchEditor = closeBatchEditor;
-window.closeWaypointEditor = closeWaypointEditor;
 window.openBatchEditor = openBatchEditor;
+window.closeWaypointEditor = closeWaypointEditor;
+window.openWaypointEditor = openWaypointEditor;
+window.generateKML = generateKML;
+window.missions = missions;
+window.waypoints = waypoints;
+
+
